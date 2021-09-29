ad13701e3b10020d453baaac43f30add50ce0fa3
ahmadkybora <ahmadkybora@gmail.com>
2021-09-29 20:33:06 +0330

    new

---
+++ middlewares/isLoggedIn.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const Token = require('../app/Models/TokenModel');
+
+async function isLoggedIn(req, res, next) {
+    const authHeader = req.headers['authorization'];
+
+    if (authHeader === null) {
+        return res.status(401).json({
+            state: true,
+            message: "unAuthorized!",
+            data: null,
+            errors: null
+        });
+    }
+
+    const token = authHeader && authHeader.split(' ')[1];
+    let result = jwt.decode(token);
+
+    const user = await Token.findOne({
+        where: {
+            userId: result.id,
+        }
+    });
+
+    if (!user) {
+        return res.status(401).json({
+            state: true,
+            message: "unAuthorized!",
+            data: null,
+            errors: null
+        });
+    } else {
+        jwt.verify(token, process.env.Access_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.status(401).json({
+                    state: true,
+                    message: "unAuthorized!",
+                    data: null,
+                    errors: null
+                });
+            } else {
+                req.body = user;
+                next()
+            }
+        })
+    }
+}
+
+module.exports = isLoggedIn;
+
